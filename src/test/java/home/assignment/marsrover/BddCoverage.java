c1bba3c0200e4e59a8e92620e55ba2a3ecd462b0
Cagdas <olgun.cagdas@gmail.com>
2018-06-10 16:20:17 +0300

    add bdd coverage and logger settings

---
+++ src/test/java/home/assignment/marsrover/BddCoverage.java
@@ -0,0 +1,11 @@
+package home.assignment.marsrover;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", strict = true)
+public class BddCoverage {
+}
